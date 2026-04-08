d7be2dd7f1852fba2c74f3473ad4cc3f6d9c73cf
SachaC <sacha.corporeau@loria.fr>
2026-04-08 17:21:11 +0200

    filled features.h file with all features IDs

--- include/features.h
+++ include/features.h
@@ -1,5 +1,5 @@
-#ifndef FEATURES_H
-#define FEATURES_H
+#ifndef __FEATURES_H
+#define __FEATURES_H
 
 
 //features binaries IDs.
@@ -7,22 +7,27 @@
 //This vector is created with binary ands and theses values Thus, we can have 128 max features, 64 flow-features and 64 packet-features.
 //example vector with 3 features : __u64 fv = F_S_PORT & F_D_PORT & F_PROTOCOL;
 
+//features range
+#define F_RANGE_PACKET  0xFFFFFFFF
+#define F_RANGE_FLOW    (0xFFFFFFFF << 64)
+
+
 //packet features
-#define F_S_PORT    1 << 0 //feature 0
-#define F_D_PORT    1 << 1 //feature 1
-#define F_PROTOCOL  1 << 2 //...
-#define F_PKT_SIZE  1 << 3
-#define F_D_IP      1 << 4
-#define F_S_IP      1 << 5
+#define F_S_PORT    (1 << 0) //feature 0
+#define F_D_PORT    (1 << 1) //feature 1
+#define F_PROTOCOL  (1 << 2) //...
+#define F_PKT_SIZE  (1 << 3)
+#define F_D_IP      (1 << 4)
+#define F_S_IP      (1 << 5)
 
 //flow features
-#define F_IAT_MEAN  1 << 0 << 64 //feature 64
-#define F_IAT_TOT   1 << 1 << 64 //feature 65
-#define F_IAT_STD   1 << 2 << 64 //...
-#define F_SIZE_MEAN 1 << 3 << 64
-#define F_SIZE_TOT  1 << 4 << 64
-#define F_SIZE_STD  1 << 5 << 64
-#define F_PKT_TOT_N 1 << 6 << 64
-#define F_PKT_TOT_S 1 << 7 << 64
+#define F_IAT_MEAN  ((1 << 0) << 64) //feature 64
+#define F_IAT_TOT   ((1 << 1) << 64) //feature 65
+#define F_IAT_STD   ((1 << 2) << 64) //...
+#define F_SIZE_MEAN ((1 << 3) << 64)
+#define F_SIZE_TOT  ((1 << 4) << 64)
+#define F_SIZE_STD  ((1 << 5) << 64)
+#define F_PKT_TOT_N ((1 << 6) << 64)
+#define F_PKT_TOT_S ((1 << 7) << 64)
 
 #endif
