b0e82070b2785899a084a7e58ad05058f179ee54
SachaC <sacha.corporeau@loria.fr>
2026-08-14 11:01:18 +0200

    benchmark 4 update : added boolean for offload & helper string

--- benchmarks/4_consec_bpftool.sh
+++ benchmarks/4_consec_bpftool.sh
@@ -2,10 +2,17 @@
 
 # This script attach & load the provided bpf objects in all modes (SKB(generic) then DRV(driver) then HW(offload))
 # the attach & loading is done via bpftool binary
+HELP="loading & attaching provided .o xdp objects while lsitening for iperf3 on the provided interface. Outputs metrics in a outputs folder (will be created automatically)
+args:
+network interface
+ip addr
+offload boolean : 1 = offload, driver and skb (netronome cards), 0 = driver and skb only (other : mlx5 ...)
+xdp .o objects"
+
 
 # retrieve net interface name from 1st arg
 if [ -z "$1" ]; then
-    echo "Please provide the network interface name as the first argument."
+    echo "$HELP"
     exit 1
 else
     IFACE=$1
@@ -13,17 +20,25 @@ fi
 
 # retrieve IP address from 2nd arg (need to be bounded to the interface already provided)
 if [ -z "$2" ]; then
-    echo "Please provide the IP address binded to the interface $IFACE."
+    echo "$HELP"
     exit 1
 else
     IP_ADDR=$2
 fi
 
-#retrieving ebpf objects from 3rd+ args
+#retrieve offload boolean from 3rd arg
 if [ -z "$3" ]; then
+    echo "$HELP"
+    exit 1
+else
+    OFFL=$3
+fi
+
+#retrieving ebpf objects from 3rd+ args
+if [ -z "$4" ]; then
     OBJS=("no_ebpf")
 else
-    shift 2
+    shift 3
     OBJS=("$@" "no_ebpf")
 fi
 #warn message because m yscript is not perfect
@@ -41,25 +56,35 @@ touch output/config.txt
 
 
 #output network configuration in config.txt file for test reliability
-echo "------ip a" >> output/config.txt
+echo "----------ip a" >> output/config.txt
 ip a >> output/config.txt
-echo "------ethtool -k" >> output/config.txt
+echo "----------ethtool -k" >> output/config.txt
 ethtool -k $IFACE >> output/config.txt
-echo "------ethtool -i" >> output/config.txt
+echo "----------ethtool -i" >> output/config.txt
 ethtool -i $IFACE >> output/config.txt
-echo "------lshw -c network" >> output/config.txt
+echo "----------lshw -c network" >> output/config.txt
 lshw -c network >> output/config.txt
 
+# add xdpoffload to modes only if OFFL is set to 1
+if [ "$OFFL" -eq 1 ] ; then
+    MODE_LIST="xdpgeneric xdpdrv xdpoffload"
+else
+    MODE_LIST="xdpgeneric xdpdrv"
+fi
+echo "-----modes list : $MODE_LIST"
+echo "-----progs list : ${OBJS[@]}"
 
-for BPF_OBJ in $OBJS; do
+
+for BPF_OBJ in "${OBJS[@]}"; do
     if [ "$BPF_OBJ" == "no_ebpf" ]; then
         echo "Benchmark without any eBPF script attached ..."
         touch "./output/iperf_$BPF_OBJ.txt"
         echo "waiting for iperf3 client to connect ..."
-        iperf3 -s -V --one-off --bind $IP_ADDR -i 10 >> "./output/iperf_$BPF_OBJ.txt"
+        iperf3 -s -V --one-off --timestamps --bind $IP_ADDR >> "./output/iperf_$BPF_OBJ.txt"
         echo "iperf3 finished running !"
     else
-        for MODE in xdpgeneric xdpdrv xdpoffload; do
+        # loop for each program & mode
+        for MODE in $MODE_LIST; do
             echo "loading & attaching object $BPF_OBJ in $MODE"
 
             #LOAD
@@ -91,7 +116,7 @@ for BPF_OBJ in $OBJS; do
             #run the server for only one connection (since each one will be outputted to a different file)
             touch "./output/iperf_$BPF_OBJ $MODE.txt"
             echo "waiting for iperf3 client to connect ..."
-            iperf3 -s -V --one-off --bind $IP_ADDR -i 10 >> "./output/iperf_$BPF_OBJ $MODE.txt"
+            iperf3 -s -V --one-off --timestamps --bind $IP_ADDR >> "./output/iperf_$BPF_OBJ $MODE.txt"
             echo "iperf3 finished running ! Detaching ebpf script"
 
             #DETACH
